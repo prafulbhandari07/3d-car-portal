ce7366b7703dafb43b65ed92bb1bc8df8b25bc72
Praful Bhandari <pbhandari490@gmail.com>
2024-12-27 17:31:44 +0530

    update: final portal render

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { Canvas } from "@react-three/fiber";
 import Experience from "./components/Experience";
 import { Text } from "@react-three/drei";
+import { act, useState } from "react";
 
 function App() {
+  const [active, setActive] = useState(null);
   return (
     <>
-      <div className="w-full h-full overflow-hidden relative">
-        <Canvas shadows camera={{ position: [0, 0, 5], fov: 40 }}>
-          <Experience />
+      <div className="w-full h-full overflow-hidden relative bg-black">
+        <Canvas shadows camera={{ position: [0, 0, 5], fov: 30 }}>
+          <Experience active={active} setActive={setActive} />
         </Canvas>
-        <div className="absolute text-black top-4 font-extrabold left-[50%]">
-          hi
+        <div
+          className="absolute w-full top-0 py-8 font-bold tracking-widest text-3xl text-white text-center bg-black/50 backdrop-blur-sm"
+          style={{ fontFamily: "'Permanent Marker', cursive" }}
+        >
+          Double Click any Portal to enter&nbsp;/&nbsp;exit it.
         </div>
       </div>
     </>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,11 +2,12 @@ import { CameraControls } from "@react-three/drei";
 import * as THREE from "three";
 import CarStage from "./CarStage";
 import McLarenModel from "./McLaren";
-import { useRef, useState, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import { useThree } from "@react-three/fiber";
+import { RB19Model } from "./Rb19";
+import { GTRModel } from "./Gtr";
 
-const Experience = () => {
-  const [active, setActive] = useState(null);
+const Experience = ({ active, setActive }) => {
   const controlsRef = useRef();
   const scene = useThree((state) => state.scene);
 
@@ -14,8 +15,16 @@ const Experience = () => {
     if (active) {
       const targetPosition = new THREE.Vector3();
       scene.getObjectByName(active).getWorldPosition(targetPosition);
-      controlsRef.current.setLookAt(0, 0, 2, targetPosition.x, targetPosition.y, targetPosition.z, true,);
-    }else{
+      controlsRef.current.setLookAt(
+        1,
+        1,
+        4,
+        targetPosition.x,
+        targetPosition.y,
+        targetPosition.z,
+        true
+      );
+    } else {
       controlsRef.current.setLookAt(0, 0, 5, 0, 0, 0, true);
     }
   }, [active]);
@@ -23,44 +32,70 @@ const Experience = () => {
   return (
     <>
       <ambientLight intensity={0.5} />
-      <CameraControls ref={controlsRef} />
+      <CameraControls
+        ref={controlsRef}
+        maxPolarAngle={Math.PI / 1.5}
+        minPolarAngle={Math.PI / 3}
+        minDistance={0.5}
+        maxDistance={6}
+      />
 
       <CarStage
         texture={"textures/tokyo.jpg"}
         position-x={-1.7}
         rotation-y={Math.PI / 8}
         preset={"city"}
-        name={"Tokyo"}
+        name={"GTR"}
         color={"#f00"}
         active={active}
         setActive={setActive}
       >
-        <McLarenModel scale={0.3} position-y={-0.5} />
+        {active ? (
+          <GTRModel
+            scale={[0.5, 0.5, 0.5]}
+            position={[-0.4, -0.4, 0.4]}
+            rotation-y={0.3}
+          />
+        ) : (
+          <GTRModel scale={0.5} position={[-0.1,-0.6,-0.2]} rotation-y={-0.02}/>
+        )}
       </CarStage>
 
       <CarStage
         texture={"textures/spa.jpg"}
         preset={"sunset"}
-        position-z={-0.3}
-        name={"Senna"}
+        position-z={0.2}
+        name={"Mclaren"}
         color={"#0f0"}
         active={active}
         setActive={setActive}
       >
-        <McLarenModel scale={0.3} position-y={-0.5} />
+        {active ? (
+          <McLarenModel
+            scale={0.4}
+            position={[0, -0.3, -0.2]}
+            rotation-y={0.1}
+          />
+        ) : (
+          <McLarenModel scale={0.3} position-y={-0.5} />
+        )}
       </CarStage>
 
       <CarStage
-        texture={"textures/spa.jpg"}
+        texture={"textures/circuit.jpg"}
         position-x={1.7}
         rotation-y={-Math.PI / 8}
         preset={"sunset"}
-        name={"Monaco"}
-        color={"#00f"}
+        name={"RB19"}
+        color={"white"}
         active={active}
         setActive={setActive}
       >
-        <McLarenModel scale={0.3} position-y={-0.5} />
+        {active ? (
+          <RB19Model scale={0.7} position={[0.3, -0.3, 0.2]} />
+        ) : (
+          <RB19Model scale={0.58} position={[0.1, -0.5, 0.5]} />
+        )}
       </CarStage>
     </>
   );
